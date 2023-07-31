97b6efba56fb0e9c98b632fd14e172b0e6aa41dd
Dina Shchobova <dina.shchobova@gmail.com>
2023-07-31 13:16:04 +0200

    feat: add image

--- src/pages/MemoriesPage.vue
+++ src/pages/MemoriesPage.vue
@@ -4,31 +4,42 @@
       <ion-item
         v-for="memory in memories"
         :key="memory.id"
-        
         :router-link="`/memories/${memory.id}`"
       >
-        {{ memory.title }}
+        
+        <ion-thumbnail slot="start">
+          <ion-img :src="memory.image" :alt="memory.title"></ion-img>
+        </ion-thumbnail>
+        <ion-label>
+          {{ memory.title }}
+        </ion-label>
+        
       </ion-item>
     </ion-list>
   </base-layout>
 </template>
 
 <script>
-import { IonList, IonItem } from "@ionic/vue";
+import { IonList, IonItem, IonImg, IonThumbnail, IonLabel } from "@ionic/vue";
 
 export default {
-  components: { IonList, IonItem },
+  components: { IonList, IonItem, IonImg, IonThumbnail, IonLabel },
 
   data() {
     return {
       memories: [
-        { id: 1, title: "Memory 1", body: "blabla 1" },
-        { id: 2, title: "Memory 2", body: "blabla 2" },
-        { id: 3, title: "Memory 3", body: "blabla 3" },
+        { id: 1, title: "Memory 1", description: "blabla 1", image: 'https://www.worldatlas.com/r/w960-q80/upload/80/50/11/shutterstock-117040402.jpg' },
+        { id: 2, title: "Memory 2", description: "blabla 2", image: 'https://www.worldatlas.com/r/w960-q80/upload/f7/26/7c/shutterstock-70804165.jpg'},
+        { id: 3, title: "Memory 3", description: "blabla 3", image: 'https://www.worldatlas.com/r/w960-q80/upload/c5/e9/cc/shutterstock-422482132.jpg'},
       ],
     };
   },
 };
 </script>
 
-<style></style>
+<style>
+ion-thumbnail {
+    --size: 56px;
+    --border-radius: 4px;
+  }
+</style>
