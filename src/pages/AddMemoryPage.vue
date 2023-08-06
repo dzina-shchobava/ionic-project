908c9e5356663b69558f499bf87aa6c22a22a6c0
Dina Shchobova <dina.shchobova@gmail.com>
2023-08-06 10:37:46 +0200

    feat: add create form

--- src/pages/AddMemoryPage.vue
+++ src/pages/AddMemoryPage.vue
@@ -1,12 +1,22 @@
 <template>
   <base-layout pageTitle="New Memory" defaultBackLink="/memories">
-
+    <create-memory-form @save-memory="saveMemory"></create-memory-form>
   </base-layout>
 </template>
 
 <script>
   export default {
-    
+    data() {
+      return {
+        memory: {},
+      }
+    },
+    methods: {
+      saveMemory(memory) {
+        this.$store.commit('addMemory', memory);
+        this.$router.replace('/memories');
+      },
+    }
   }
 </script>
 
